4915f014f800c649bc55d4871ca8442c6cb0c3dd
Saurabh Bansal <saurabh.bansal@marinsoftware.com>
2016-07-15 14:59:11 -0700

    edit modal working

--- public/google.js
+++ public/google.js
@@ -1,24 +1,27 @@
 var TOOL = [];
-var shipDetails = [
-    { Name: 'Hanari Carnes', City: 'Brazil' },
-    { Name: 'Split Rail Beer & Ale', City: 'USA' },
-    { Name: 'Ricardo Adocicados', City: 'Brazil' }
-];
-var flowDiagram ;
+var shipDetails = [{
+    Name: 'Hanari Carnes',
+    City: 'Brazil'
+}, {
+    Name: 'Split Rail Beer & Ale',
+    City: 'USA'
+}, {
+    Name: 'Ricardo Adocicados',
+    City: 'Brazil'
+}];
+var flowDiagram;
 
 String.prototype.format = function(placeholders) {
     if ($.isArray(placeholders)) {
         var args = arguments;
         return this.replace(/{(\d+)}/g, function(match, number) {
-            return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-                ;
+            return typeof args[number] != 'undefined' ?
+                args[number] :
+                match;
         });
-    }
-    else { //Object
+    } else { //Object
         var s = this;
-        for(var propertyName in placeholders) {
+        for (var propertyName in placeholders) {
             var re = new RegExp('{' + propertyName + '}', 'gm');
             s = s.replace(re, placeholders[propertyName]);
         }
@@ -35,8 +38,8 @@ String.prototype.format = function(placeholders) {
  */
 function guid() {
     function _p8(s) {
-        var p = (Math.random().toString(16)+"000000000").substr(2,8);
-        return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+        var p = (Math.random().toString(16) + "000000000").substr(2, 8);
+        return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
     }
     return _p8() + _p8(true) + _p8(true) + _p8();
 }
@@ -44,130 +47,125 @@ function guid() {
 /**
  * Edit Node now , add more fields or whatever
  */
-function editNode(nodeId){
-    alert('editing ' + nodeId);
-    // parse node tye from nodeID
-    var nodeType = nodeId.split('_')[0];
-    // get data from server if exists by node_id
-    // use that data
-    // save again after edit
-    getNodeDataById(nodeId)
-
-    // get nodeID specific data from users--> tool --> nodes
-    // update properties on node
-    //nodes[0].fields.push('field4');
-    //drawNode(nodes[0],nodeId)
-
-    //            $("#form").alpaca({
-//                "schema": {
-//                    "title":"User Feedback",
-//                    "description":"What do you think about Alpaca?",
-//                    "type":"object",
-//                    "properties": {
-//                        "name": {
-//                            "type":"string",
-//                            "title":"Name"
-//                        },
-//                        "feedback": {
-//                            "type":"string",
-//                            "title":"Feedback"
-//                        },
-//                        "ranking": {
-//                            "type":"string",
-//                            "title":"Ranking",
-//                            "enum":['excellent','ok','so so']
-//                        }
-//                    }
-//                }
-//            });
-//
-//
-//            $('#myModal').dialog({
-//                autoOpen:true,
-//                my: "center",
-//                at: "center",
-//                of: window
-//            });
-
-}
-
-function addField(nodeId,node,field){
-    var rowId = nodeId + '_' + field.name
-    var tableRow = '<tr id=' + rowId + '>' +
-        '<td align="center">'+field.name+'</td>'+
-        '</tr>'
-    $('#' + nodeId + " .table").append(tableRow);
-    setEndPoint(rowId,node)
-}
-
-function addSourceEndPoint(rowId){
-    jsPlumb.addEndpoint(rowId, {
-        anchors:['Right'],
-        isSource:true,
-        isTarget:false,
-        endpoint : ["Rectangle",{width:15, height:15}],
-        endpointStyle : {fillStyle:"orange", outlineColor:"orange", outlineWidth:1 },
-        hoverPaintStyle:{ fillStyle:"yellow"},
-        maxConnections:10
+function editNode(nodeId) {
+    var thisNode = _.find(TOOL.nodes, function(n) {
+        return n.nodeId === nodeId
+    })
+    console.log('this node', thisNode);
+    $.get("assests/bootui.html?time=" + (new Date()).getTime(), function(data) {
+        $('body').append(data)
+        $('body').append(data);
+        $("#form").alpaca({
+            "schema": thisNode.schema,
+            "options":thisNode.options,
+            "data":this.data
+        });
+        $('#myModal').dialog({
+            autoOpen: true,
+            my: "center",
+            at: "center",
+            of: window
+        });
     });
 }
 
-function addTargetEndPoint(rowId){
+function addField(nodeId, node, field) {
+    var rowId = nodeId + '_' + field.name
+    var tableRow = '<tr id=' + rowId + '>' +
+        '<td align="center">' + field.name + '</td>' +
+        '</tr>'
+    $('#' + nodeId + " .table").append(tableRow);
+    setEndPoint(rowId, node)
+}
+
+function addSourceEndPoint(rowId) {
     jsPlumb.addEndpoint(rowId, {
-        anchors:['Left'],
-        isSource:false,
-        isTarget:true,
-        endpoint : ["Rectangle",{width:12, height:12}],
-        endpointStyle : {fillStyle:"blue", outlineColor:"blue", outlineWidth:1},
-        hoverPaintStyle:{fillStyle:"lightblue"},
-        maxConnections:1,
-        onMaxConnections: function(){
+        anchors: ['Right'],
+        isSource: true,
+        isTarget: false,
+        endpoint: ["Rectangle", {
+            width: 15,
+            height: 15
+        }],
+        endpointStyle: {
+            fillStyle: "orange",
+            outlineColor: "orange",
+            outlineWidth: 1
+        },
+        hoverPaintStyle: {
+            fillStyle: "yellow"
+        },
+        maxConnections: 10
+    });
+}
+
+function addTargetEndPoint(rowId) {
+    jsPlumb.addEndpoint(rowId, {
+        anchors: ['Left'],
+        isSource: false,
+        isTarget: true,
+        endpoint: ["Rectangle", {
+            width: 12,
+            height: 12
+        }],
+        endpointStyle: {
+            fillStyle: "blue",
+            outlineColor: "blue",
+            outlineWidth: 1
+        },
+        hoverPaintStyle: {
+            fillStyle: "lightblue"
+        },
+        maxConnections: 1,
+        onMaxConnections: function() {
             console.log('max connection limit reached')
         }
     });
 }
 
-function setEndPoint(rowId,node){
-    if (node.type === 'transform'){
+function setEndPoint(rowId, node) {
+    if (node.type === 'transform') {
         addSourceEndPoint(rowId);
         addTargetEndPoint(rowId);
     }
-    if (node.type === 'source'){
+    if (node.type === 'source') {
         addSourceEndPoint(rowId);
     }
-    if (node.type === 'target'){
+    if (node.type === 'target') {
         addTargetEndPoint(rowId);
     }
 }
 
-function drawNode(node,cb){
-    console.log('drawing node',node,node.positionX,node.positionY)
+function drawNode(node, cb) {
+    console.log('drawing node', node, node.positionX, node.positionY)
     $.get("assests/node.html?time=" + (new Date()).getTime(), function(data) {
         var nodeId = node.nodeId ? node.nodeId : (node.name + '_' + guid());
-        console.log('appending',data.format({
+        console.log('appending', data.format({
             node_id: nodeId,
-            node_name:node.name,
-            image:node.image
+            node_name: node.name,
+            image: node.image
         }))
         $('.canvas').append(data.format({
             node_id: nodeId,
-            node_name:node.name,
-            image:node.image
+            node_name: node.name,
+            image: node.image
         }));
-        $('#'+nodeId).css('left', (node.positionX ? node.positionX : 30) + 'px');
-        $('#'+nodeId).css('top', (node.positionY ? node.positionY : 30) + 'px');
-        jsPlumb.draggable(nodeId,{containment:"parent"});
-        node.fields.forEach(function(field){
-            addField(nodeId,node,field);
+        $('#' + nodeId).css('left', (node.positionX ? node.positionX : 30) + 'px');
+        $('#' + nodeId).css('top', (node.positionY ? node.positionY : 30) + 'px');
+        jsPlumb.draggable(nodeId, {
+            containment: "parent"
+        });
+        node.fields.forEach(function(field) {
+            addField(nodeId, node, field);
         });
         cb()
     });
 }
 
 // save canvas as well as nodes
-function save(){
+function save() {
     var nodes = []
-    $(".tableDesign").each(function (idx, elem) {
+    $(".tableDesign").each(function(idx, elem) {
         var $elem = $(elem);
         nodes.push({
             nodeId: $elem.attr('id'),
@@ -178,18 +176,21 @@ function save(){
     });
 
     var connections = [];
-    $.each(jsPlumb.getConnections(), function (idx, connection) {
+    $.each(jsPlumb.getConnections(), function(idx, connection) {
         connections.push({
             connectionId: connection.id,
             pageSourceId: connection.sourceId,
             pageTargetId: connection.targetId,
             anchors: $.map(connection.endpoints, function(endpoint) {
-                return [[endpoint.anchor.x,
-                    endpoint.anchor.y,
-                    endpoint.anchor.orientation[0],
-                    endpoint.anchor.orientation[1],
-                    endpoint.anchor.offsets[0],
-                    endpoint.anchor.offsets[1]]];
+                return [
+                    [endpoint.anchor.x,
+                        endpoint.anchor.y,
+                        endpoint.anchor.orientation[0],
+                        endpoint.anchor.orientation[1],
+                        endpoint.anchor.offsets[0],
+                        endpoint.anchor.offsets[1]
+                    ]
+                ];
 
             })
         });
@@ -203,53 +204,36 @@ function save(){
 
     var flowChartJson = JSON.stringify(flowChart);
     TOOL.canvas = flowChartJson
-    $.ajax({type: "POST",
+    $.ajax({
+        type: "POST",
         url: "/tool",
-        data:
-        {
-            toolName:"google",
+        data: {
+            toolName: "google",
             canvas: TOOL.canvas,
-            settings:TOOL.settings,
-            nodes:TOOL.nodes
+            settings: TOOL.settings,
+            nodes: TOOL.nodes
         },
-        success:function(result) {
-        alert('saved --' + result);
-        }
-    });
-
-}
-
-function getNodeDataById(nodeId){
-    $.ajax({type: "GET",
-        url: "/getstate",
-        data:
-        {
-            toolName:"google",
-            nodeId:nodeId 
-        },
-        success:function(result) {
+        success: function(result) {
             alert('saved --' + result);
         }
     });
-}
-
-// load combines physical with node data
-// and then draw nodes and connections
-function load(){
 
 }
+
 
 // get node data and fields from server by user
 // if has use that else use from raw node
 
-$('#btnSave').click(function(){
+$('#btnSave').click(function() {
     save();
 })
 
-$('#btnLoad').click(function(){
-    jsPlumb.load({savedObj : flowDiagram, containerSelector : "#canvas"});
+$('#btnLoad').click(function() {
+    jsPlumb.load({
+        savedObj: flowDiagram,
+        containerSelector: "#canvas"
+    });
 })
-
 
 jsPlumb.ready(function() {
     $("#outterSplitter").ejSplitter({
@@ -257,90 +241,105 @@ jsPlumb.ready(function() {
         //width: "100%",
         orientation: ej.Orientation.Horizontal,
         enableAutoResize: true,
-        properties: [{ collapsible: true }]
+        properties: [{
+            collapsible: true
+        }]
     });
     $('#Grid').ejGrid({
         dataSource: shipDetails
     });
     jsPlumb.importDefaults({
         Connector: ["Straight"],
-        PaintStyle: {strokeStyle: "rgba(50,50,50,1)", lineWidth:2.5},
-        HoverPaintStyle: { lineWidth:3.5,
+        PaintStyle: {
+            strokeStyle: "rgba(50,50,50,1)",
+            lineWidth: 2.5
+        },
+        HoverPaintStyle: {
+            lineWidth: 3.5,
             strokeStyle: 'rgba(0, 17, 255, 1)'
         },
         ConnectionOverlays: [
-            [ "Arrow", {
-                width:10,
-                height:10, location:0.5, id:"arrow",
-                events:{
-                    click: function(){
+            ["Arrow", {
+                width: 10,
+                height: 10,
+                location: 0.5,
+                id: "arrow",
+                events: {
+                    click: function() {
                         //alert('connection overlay clicked')
                         console.log('connection overlay clicked')
                     },
                 }
-            } ],
+            }],
             // this is how you add label to arrow
             //[ "Label", { label:"Relationship", id:"lblPrimary_" + 'casasc' } ]
         ]
     })
     jsPlumb.setContainer("canvas");
-    jsPlumb.bind("connection", function (info, originalEvent) {
+    jsPlumb.bind("connection", function(info, originalEvent) {
         //alert("connected "+info.sourceId+" and "+info.targetId + " via "+info.connection);
         var connection1 = info.connection;
         connection1.bind("click", function(connection, originalEvent) {
             //alert("you clicked on "+ connection);
-            connection.setPaintStyle({strokeStyle:"rgba(0, 17, 255, 1)", lineWidth:3.5});
+            connection.setPaintStyle({
+                strokeStyle: "rgba(0, 17, 255, 1)",
+                lineWidth: 3.5
+            });
             keyboardJS.on('del', function(event, keys, keyComboStr) {
                 jsPlumb.detach(connection)
             })
             keyboardJS.on('esc', function(event, keys, keyComboStr) {
-                connection.setPaintStyle({strokeStyle: "rgba(50,50,50,1)", lineWidth:2.5});
+                connection.setPaintStyle({
+                    strokeStyle: "rgba(50,50,50,1)",
+                    lineWidth: 2.5
+                });
             })
 
         });
     });
-    $.ajax({type: "GET",
+    $.ajax({
+        type: "GET",
         url: "/tool",
-        data:
-        {
-            toolName:"google"
+        data: {
+            toolName: "google"
         },
-        success:function(result) {
-            console.log('result',result)
+        success: function(result) {
+            console.log('result', result)
             TOOL = result;
-            if (TOOL.canvas){
+            if (TOOL.canvas) {
                 console.log('loading from existing canvas')
                 // draw nodes
                 var canvasObject = JSON.parse(TOOL.canvas)
                 async.waterfall([
-                 function(cb){
-                     async.eachSeries(canvasObject.nodes,function(cn,cb){
-                         var onode = _.find(TOOL.nodes,function(n){return n.name === cn.nodeName})
-                         if (onode){
-                             onode.nodeId  = cn.nodeId;
-                             onode.positionX = cn.positionX;
-                             onode.positionY = cn.positionY;
-                             drawNode(onode,cb);
-                         }
-                     },cb)
-                 }  ,
-                 function(cb){
-                        alert('on to connectors')
+                    function(cb) {
+                        async.eachSeries(canvasObject.nodes, function(cn, cb) {
+                            var onode = _.find(TOOL.nodes, function(n) {
+                                return n.name === cn.nodeName
+                            })
+                            if (onode) {
+                                onode.nodeId = cn.nodeId;
+                                onode.positionX = cn.positionX;
+                                onode.positionY = cn.positionY;
+                                drawNode(onode, cb);
+                            }
+                        }, cb)
+                    },
+                    function(cb) {
                         // connect existing connectors
                         var connections = canvasObject.connections;
-                        connections.forEach(function(c){
+                        connections.forEach(function(c) {
                             jsPlumb.connect({
                                 source: c.pageSourceId,
                                 target: c.pageTargetId,
-                                anchors:c.anchors
+                                anchors: c.anchors
                             });
                         });
-                     cb()
+                        cb()
                     }
                 ])
             } else {
                 // first time drawing canvas
-                TOOL.nodes.forEach(function(n){
+                TOOL.nodes.forEach(function(n) {
                     drawNode(n)
                 });
             }
